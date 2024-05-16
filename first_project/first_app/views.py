423fd309223a71f00117a5bb74fa2b4cd87cd7ba
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-05-16 09:31:33 +0530

    code commit
    data binding done

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from first_app.models import Topic,WebPage,AccessRecord
 # Create your views here.
 
 # def index(request):
 #     return HttpResponse("Hello World")
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
     my_dict = {'name':"Mangesh"}
-    return render(request,'index.html',context=my_dict)
+    return render(request,'index.html',context=date_dict)
